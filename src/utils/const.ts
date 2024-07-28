e38ef8adf23de0bca8c137d71e4a2f6a110b2cbb
13934 <1393446061@qq.com>
2024-07-29 01:49:05 +0800

    feat: 切换环境实现

---
+++ src/utils/const.ts
@@ -0,0 +1,20 @@
+export const enum StorageKeys {
+    CommunityId = "communityId",
+    UserId = "userId",
+    AccessToken = "accessToken",
+    HistoryCampuses = "historyCampuses",
+    IdSelected = "idSelected",
+    NameSelected = "nameSelected",
+    AvatarSelected = "avatarSelected",
+    DraftMoment = "draftMoment",
+    DraftPost = "draftPost",
+    BackendEnv = "backendEnv",
+    BackendLane = "backendLane",
+    EnabledDebug = "enableDebug",
+    HistorySearchText = "historySearchText"
+}
+
+export const enum BackendEnv {
+    Product = "pro",
+    Test = "test"
+}
